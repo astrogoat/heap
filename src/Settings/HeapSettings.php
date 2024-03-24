6be63a581a751648f9250cdf392971ecf36acffa
Laura Tonning <tonning@gmail.com>
2024-03-24 21:18:58 +0100

    Clean up

--- src/Settings/HeapSettings.php
+++ src/Settings/HeapSettings.php
@@ -8,21 +8,10 @@ use Helix\Lego\Settings\AppSettings;
 class HeapSettings extends AppSettings
 {
     public string $app_id;
-    // public string $access_token;
 
     protected array $rules = [
         'app_id' => ['required'],
-        // 'access_token' => ['required'],
     ];
-
-    protected static array $actions = [
-        // HeapAction::class,
-    ];
-
-    // public static function encrypted(): array
-    // {
-    //     return ['access_token'];
-    // }
 
     public function description(): string
     {
